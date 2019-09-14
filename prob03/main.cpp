3fa82c14da38d9d3c510f04c0d2c7fa16998681c
Kbliaya <kbliaya@csu.fullerton.edu>
2019-09-14 14:01:06 -0700

    created a program to calculate the sales prediction based on last year's sales and the amount increase as constants.

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,22 @@
+// Name: Kaylee Bliaya
 // Sales prediction
+
+#include <iostream>
+
+int main ()
+{
+  const long SALES1 = 2103419277;
+  const double INCREASE = 0.18;
+  long sales2;
+
+  // Display the sales for last year (2018).
+  std::cout << "Last year's sales were " << SALES1 << '\n';
+
+  //Calculate the sales prediction for next year.
+  sales2 = SALES1 + (SALES1 * INCREASE);
+
+  //Display the sales prediction for next year.
+  std::cout << "This year's sales prediction: " <<static_cast<long>(sales2) << '\n';
+
+  return 0;
+}
